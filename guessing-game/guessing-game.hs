7faad028a42eda1ad0214de3538ea6a86c904901
Alexander Harley <moh_Harper@msn.com>
2017-04-25 23:08:32 +0100

    Progress on Guessing Game

--- guessing-game/guessing-game.hs
+++ guessing-game/guessing-game.hs
@@ -8,10 +8,22 @@ main = do
     secretNumber <- getRandomNumber
     putStrLn $ "Your secret number is: " ++ show (head secretNumber)
 
-    guess <- getLine
-    putStrLn $ "You guessed: " ++ guess
+    guessResult <- guessLogic $ head secretNumber
+    putStrLn guessResult
 
 getRandomNumber :: IO [Int]
 getRandomNumber = do
     randomNumber <- getStdGen
-    return $ take 1 (randomRs (1, 100) randomNumber)
+    return $ take 1 $ randomRs (1, 100) randomNumber
+
+guessLogic :: Int -> IO [Char]
+guessLogic secret = do
+    guess <- getLine
+    putStrLn $ "You guessed: " ++ guess
+    return $ checkNumber (read guess :: Int) secret
+
+checkNumber :: Int -> Int -> [Char]
+checkNumber x y
+    | x < y  = "You guessed too low"
+    | x > y  = "You guessed too high"
+    | x == y = "You're correct!"
