7d6400648ae62bf802f4e2f9a89ba6651284342b
AlexanderHarley <moh_Harper@msn.com>
2017-04-26 13:56:57 +0100

    Guessing game completed

--- guessing-game/guessing-game.hs
+++ guessing-game/guessing-game.hs
@@ -1,29 +1,30 @@
 import System.Random
+import Control.Monad
 
 main :: IO ()
 main = do
     putStrLn "Guess the number!"
-    putStrLn "Please input your guess."
 
     secretNumber <- getRandomNumber
-    putStrLn $ "Your secret number is: " ++ show (head secretNumber)
-
-    guessResult <- guessLogic $ head secretNumber
-    putStrLn guessResult
+    -- putStrLn $ "Your secret number is: " ++ show (head secretNumber)
+    guessLogic $ head secretNumber
 
 getRandomNumber :: IO [Int]
 getRandomNumber = do
     randomNumber <- getStdGen
     return $ take 1 $ randomRs (1, 100) randomNumber
 
-guessLogic :: Int -> IO [Char]
+guessLogic :: Int -> IO ()
 guessLogic secret = do
+    putStrLn "Please input your guess."
     guess <- getLine
     putStrLn $ "You guessed: " ++ guess
-    return $ checkNumber (read guess :: Int) secret
+    putStrLn $ checkNumber (read guess :: Int) secret
+    when ((read guess :: Int) /= secret) $ guessLogic secret
 
-checkNumber :: Int -> Int -> [Char]
+checkNumber :: Int -> Int -> String
 checkNumber x y
-    | x < y  = "You guessed too low"
-    | x > y  = "You guessed too high"
-    | x == y = "You're correct!"
+    | x < y     = "You guessed too low"
+    | x > y     = "You guessed too high"
+    | x == y    = "You're correct!"
+    | otherwise = error "Can't calculate difference"
